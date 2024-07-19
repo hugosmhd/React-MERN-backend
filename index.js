2c313e07fe17bca68ebf56f43b3959498046c8be
hugosmhd <hugo.kio40@gmail.com>
2024-07-19 09:30:16 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors())
+
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+// app.get('/', (req, res) => {
+//     console.log('se requiere /');
+//     res.json({
+//         ok: true
+//     })
+// })
+app.use('/api/auth', require('./routes/auth') );
+app.use('/api/events', require('./routes/events') );
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
